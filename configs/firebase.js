caff7d9082cf6a038e709375e5c7eb6d9256f4d0
mabs-ceo <codepreneurceo@gmail.com>
2025-04-09 19:07:47 +0800

    test commit

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,6 +1,7 @@
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json'); // download from Firebase Console
+// const serviceAccount = require('../serviceAccountKey.json'); // download from Firebase Console
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json'); // download from Firebase Console
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
