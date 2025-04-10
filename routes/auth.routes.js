b46643eba1bc70b02707b53115d00e6bd945f58d
mabs-ceo <codepreneurceo@gmail.com>
2025-04-10 12:59:07 +0800

    redo client server

--- routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { RegisterProvider } = require('../controller/providers.controller');
-const router = express.Router();
-
-router.post('/register',RegisterProvider);
-
-module.exports = router;
--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getNotifications, createNotifications } = require('../controller/notification.controller');
+const { getNotifications } = require('../controller/notification.controller');
 const router = express.Router();
 
 
 router.get('/janaza', getNotifications)
-router.post('/janaza', createNotifications)
+
 
 
 
